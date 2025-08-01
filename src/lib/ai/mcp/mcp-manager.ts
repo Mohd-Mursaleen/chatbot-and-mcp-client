8d53ef32cddeac65ee7b7779dca64de46c9dbcc3
Mohd Mursaleen <mhdpope@gmail.com>
2025-08-01 14:12:16 +0530

    remove db based mcp

--- src/lib/ai/mcp/mcp-manager.ts
+++ src/lib/ai/mcp/mcp-manager.ts
@@ -1,10 +1,8 @@
-import { createDbBasedMCPConfigsStorage } from "./db-mcp-config-storage";
 import { createFileBasedMCPConfigsStorage } from "./fb-mcp-config-storage";
 import {
   createMCPClientsManager,
   type MCPClientsManager,
 } from "./create-mcp-clients-manager";
-import { FILE_BASED_MCP_CONFIG } from "lib/const";
 declare global {
   // eslint-disable-next-line no-var
   var __mcpClientsManager__: MCPClientsManager;
@@ -12,9 +10,9 @@ declare global {
 
 if (!globalThis.__mcpClientsManager__) {
   // Choose the appropriate storage implementation based on environment
-  const storage = FILE_BASED_MCP_CONFIG
-    ? createFileBasedMCPConfigsStorage()
-    : createDbBasedMCPConfigsStorage();
+  const storage = createFileBasedMCPConfigsStorage()
+    
+    
   globalThis.__mcpClientsManager__ = createMCPClientsManager(storage);
 }
 
